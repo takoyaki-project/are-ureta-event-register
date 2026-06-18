a89cc51bb0d46d8f6fde53c73126660b7b600b21
takoyaki-project <ando.mary@gmail.com>
2026-06-18 14:27:29 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'are-ureta-v1-12-cache-001';
+const CACHE_NAME = 'are-ureta-v1-12-1-cache-001';
 
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
-  './index.html?v=1.12',
+  './index.html?v=1.12.1',
   './manifest.json',
   './sw.js',
   './icon-192.png',
